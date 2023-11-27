1f218029e5d7a348c79145699be76758af421090
mshirish <104812313+mshirish@users.noreply.github.com>
2023-11-27 13:42:13 +0545

    new-commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import Calculator from "./Projects/Calculator";
+import Navbar from "./Navbar";
+import {Route, Routes} from 'react-router-dom';
+import HomePage from "./Projects/HomePage";
+import BMI from "./Projects/BMI";
+import TodoList from './Projects/TodoList';
+import Notes from "./Projects/Notes";
+import TextUtil from "./Projects/TextUtil";
+
 
 function App() {
+  
+  const [mode, setMode] = useState('light')
+
+  const toggleMode = () => {
+    if(mode === 'light') {
+      setMode('dark')
+      document.body.style.backgroundColor = '#2B2730'
+    } else {
+      setMode('light')
+      document.body.style.backgroundColor = '#F6F4EB'
+    }
+  }
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar mode = {mode} toggleMode = {toggleMode}/>
+    <Routes>
+      <Route path = "/" element = {<HomePage />} />
+      <Route path = "/bmicalculator" element = {<BMI mode = {mode}/>} />
+      <Route path = "/calculator" element = {<Calculator />} />
+      <Route path = "/todolist" element = {<TodoList mode = {mode}/>} />
+      <Route path = "/notes" element = {<Notes mode = {mode}/>} />
+      <Route path = "/textutil" element = {<TextUtil mode = {mode}/>} />
+    </Routes>
+    </>
   );
 }
 
